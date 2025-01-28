29e06de9edd7d1b3e558cf6d8e5bd26fe43a52fe
Juliet <julietmcaulfield@gmail.com>
2025-01-28 17:49:39 -0500

    create intake coral command

---
+++ src/main/java/frc/robot/commands/IntakeCoral.java
@@ -0,0 +1,43 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.subsystems.CoralSubsystem;
+
+/* You should consider using the more terse Command factories API instead https://docs.wpilib.org/en/stable/docs/software/commandbased/organizing-command-based.html#defining-commands */
+public class IntakeCoral extends Command {
+
+  private CoralSubsystem coralSub;
+  /** Creates a new IntakeCoral. */
+  public IntakeCoral() {
+    // Use addRequirements() here to declare subsystem dependencies.
+    coralSub = CoralSubsystem.getInstance();
+    addRequirements(coralSub);
+  }
+
+  // Called when the command is initially scheduled.
+  @Override
+  public void initialize() {}
+
+  // Called every time the scheduler runs while the command is scheduled.
+  @Override
+  public void execute() {
+    // Tune speed later, through trial & error
+    coralSub.runIntake(.5);
+  }
+
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) {
+    coralSub.stopIntake();
+  }
+
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+    return coralSub.getPhotoElectric();
+  }
+}
--- src/main/java/frc/robot/subsystems/CoralSubsystem.java
+++ src/main/java/frc/robot/subsystems/CoralSubsystem.java
@@ -12,14 +12,38 @@ import com.revrobotics.spark.config.SparkMaxConfig;
 
 import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj.Encoder;
-import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class CoralSubsystem extends SubsystemBase {
   /** Creates a new CoralSubsystem. */
+  public static CoralSubsystem instance;
+  private final SparkMax coralMotor = new SparkMax(14, MotorType.kBrushless);
+  private final RelativeEncoder coralEncoder = coralMotor.getEncoder();
+  private final DigitalInput coralPhotoElectric = new DigitalInput(6); 
+
   public CoralSubsystem() {}
+
+ public void runIntake(double speed) {
+    coralMotor.set(speed);
+  }
+
+  public void stopIntake() {
+    coralMotor.set(0);
+  }
+
+  public boolean getPhotoElectric() {
+    return coralPhotoElectric.get();
+  }
+
+  public static CoralSubsystem getInstance() {
+    if (instance == null) {
+      instance = new CoralSubsystem();
+    }
+    return instance;
+  }
 
   @Override
   public void periodic() {
     // This method will be called once per scheduler run
-  }
+  } 
+
 }
